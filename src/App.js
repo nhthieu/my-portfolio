066b953f1898979a665806753b02e300a76d8c59
Hiếu Nguyễn <nguyenhotrunghieu0106@gmail.com>
2022-11-24 22:44:44 +0700

    Fix preloader

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { useState, useEffect } from 'react';
 import Preloader from './components/Preloader';
 
 function App() {
+  const [hidePreloader, setHidePreloader] = useState(false);
   const [showPreloader, setShowPreloader] = useState(true);
+  const fadeOutTime = 2000;
 
   useEffect(() => {
     document.title = "My Portfolio";
@@ -12,24 +14,25 @@ function App() {
   // Set a timeout to hide the preloader after some time
   useEffect(() => {
     const id = setTimeout(() => {
+      setHidePreloader(true);
+    }, fadeOutTime);
+
+    const id2 = setTimeout(() => {
       setShowPreloader(false);
-    }, 3200);
+    }, fadeOutTime + 1000);
 
     return () => {
       clearTimeout(id);
+      clearTimeout(id2);
     }
   }, []);
 
   return (
     <div className="App">
-      {
-        showPreloader
-          ? <Preloader />
-          :
-          <section className="main">
-            <h1 className='test'>Full Website here</h1>
-          </section>
-      }
+      {showPreloader && <Preloader hidePreloader={hidePreloader} />}
+      <section className="main">
+        <h1 className='test'>Full Website here</h1>
+      </section>
     </div>
   );
 }
